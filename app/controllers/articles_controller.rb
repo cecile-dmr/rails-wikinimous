237bc07e38ef1f8c5fc4303846f0636348677999
Cecile Demoor <cecile.demoor@gmail.com>
2025-02-25 16:55:35 +0100

    debut de l'exo 3 sur le blog d'article

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,20 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params[:restaurant])
+    @article.save # Will raise ActiveModel::ForbiddenAttributesError
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,13 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require "faker"
+
+10.times do
+  article = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Quote.famous_last_words,
+  )
+  article.save
+end
